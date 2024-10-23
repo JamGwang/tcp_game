6147f4caa642e9d32e94bd0f00f14edc935a6bb6
JamGwang <twr9680@naver.com>
2024-10-23 12:00:11 +0900

    서버 기본 세팅, 패킷 구조, 이벤트 핸들링, 세션 관리, 에러 핸들링

---
+++ src/handlers/user/initial.handler.js
@@ -0,0 +1,28 @@
+//가장 최초에 접속했을 때 유저의 정보를 처리할 핸들러
+import { addUser } from '../../session/user.session.js';
+import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+import { handleError } from '../../utils/error/errorHandler.js';
+
+const initialHandler = async ({ socket, userId, payload }) => {
+    try {
+        const { deviceId } = payload;
+
+        addUser(socket, deviceId);
+
+        // 유저 정보 응답 생성
+        const initialResponse = createResponse(
+            HANDLER_IDS.INITIAL,
+            RESPONSE_SUCCESS_CODE,
+            { userId: deviceId },
+            deviceId,
+        );
+
+        // 소켓을 통해 클라이언트에게 응답 메시지 전송
+        socket.write(initialResponse);
+    } catch (e) {
+        handleError(socket, e);
+    }
+};
+
+export default initialHandler;
---
+++ src/protobuf/packetNames.js
@@ -0,0 +1,13 @@
+// 프로토버프 매핑
+// 프로토 확장자에 접근하여 프로토 load를 하기 위함
+export const packetNames = {
+    common: {
+        Packet: 'common.Packet',
+    },
+    initial: {
+        InitialPacket: 'initial.InitialPacket',
+    },
+    response: {
+        Response: 'response.Response',
+    },
+};
